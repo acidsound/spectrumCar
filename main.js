2dceb3bb0bceb0b09eebb206fe7c54da21c1f63c
spectrum <spectrick@gmail.com>
2015-12-24 09:09:17 +0900

    fix send message

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ var s=function(d) {
 var dq=function(s,ev,c) { return document.querySelector(s).addEventListener(ev, c); };
 window.addEventListener('DOMContentLoaded', function() {
   console.log("ready");
-  dq("#fwd>.left", 'touchstart', function(e) { s("97"); });
+  dq("#fwd>.left", 'touchstart', function(e) { s("59"); });
   dq("#fwd>.middle", 'touchstart', function(e) { s("99"); });
-  dq("#fwd>.right", 'touchstart', function(e) { s("79"); });
-  dq("#mid>.left", 'touchstart', function(e) { s("95"); });
+  dq("#fwd>.right", 'touchstart', function(e) { s("95"); });
+  dq("#mid>.left", 'touchstart', function(e) { s("09"); });
   dq("#mid>.middle", 'touchstart', function(e) { s("55"); });
-  dq("#mid>.right", 'touchstart', function(e) { s("59"); });
-  dq("#bwd>.left", 'touchstart', function(e) { s("02"); });
+  dq("#mid>.right", 'touchstart', function(e) { s("90"); });
+  dq("#bwd>.left", 'touchstart', function(e) { s("50"); });
   dq("#bwd>.middle", 'touchstart', function(e) { s("00"); });
-  dq("#bwd>.right", 'touchstart', function(e) { s("20"); });
+  dq("#bwd>.right", 'touchstart', function(e) { s("05"); });
   var out=document.querySelectorAll("#panel>div>div");
   for (var k in out) {
     (function(qq, oq) {
